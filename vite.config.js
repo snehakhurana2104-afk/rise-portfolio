76633cc277631d1754d951ff24a9d6ee25b040a3
Sneha <sneha.khurana2104@gmail.com>
2026-06-09 17:24:07 +0530

    Fixing project structure and setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './', // Vite ko batata hai ki yahi main folder hai
 })
